7a27346c6696835ab0a715e1ea3101a33386a790
Meowu <474384902@qq.com>
2022-02-16 01:18:02 +0800

    add crate open dep and use it.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-// use std::process::{exit, Command};
+use open;
 use open_repo::*;
 
 fn main() {
     check_git();
     let remote_url = get_remote_url();
-    println!("remote url -> {}", remote_url);
-    println!("final url -> {}", generate_url(&remote_url));
+    let url = generate_url(&remote_url);
+    open::that(&url);
 }
